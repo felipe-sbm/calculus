5ec3801a02bb345c49ced7660ffa55830e241d25
Felipe Samuel <129466661+felipe-sbm@users.noreply.github.com>
2024-04-25 11:36:38 -0300

    Adjusting for tomorow.

--- spiral-calculus/Form1.cs
+++ spiral-calculus/Form1.cs
@@ -10,11 +10,71 @@ using System.Windows.Forms;
 
 namespace spiral_calculus
 {
-    public partial class Form1 : Form
+    public partial class Calculus : Form
     {
-        public Form1()
+        public Calculus()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button33_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button31_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button32_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button25_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button26_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
--- spiral-calculus/Program.cs
+++ spiral-calculus/Program.cs
@@ -16,7 +16,7 @@ namespace spiral_calculus
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Calculus());
         }
     }
 }
